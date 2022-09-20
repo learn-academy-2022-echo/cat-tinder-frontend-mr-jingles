08c18a65c94a33873ba1b3b32fcc7341c1938501
fpsolis219 <fpsolis21995@gmail.com>
2022-09-19 17:20:26 -0700

    progress

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {usetState} from 'react'
+import { Routes, Route } from "react-router-dom"
+import Header from './compnonents/Header'
+import Footer from './compnonents/Footer'
+import Home from './pages/Home'
+import CatIndex from './pages/CatIndex'
+import CatShow from './pages/CatShow'
+import CatNew from './pages/CatNew'
+import CatEdit from './pages/CatEdit'
+import NotFound from './pages/NotFound'
+import mockCats from "./mockCats"
+
+
 
 function App() {
+  const [cats, setCats] = useState(mockCats)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   <Header/>
+   <Routes>
+    <Route path="/" element={<Home/>} />
+    <Route path="/catindex" element={<CatIndex/>}/>
+    <Route path="/catshow" element={<CatShow/>}/>
+    <Route path="/catnew" element={<CatNew/>}/>
+    <Route path="/catedit" element={<CatEdit/>}/>
+    <Route path="/*" element={<NotFound/>}/>
+   </Routes>
+   <Footer/>
+    </>
   );
 }
 
