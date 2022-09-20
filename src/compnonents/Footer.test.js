9934957dd8680a842217f2da8d173ff55969403f
Francisco Solis <106039705+fpsolis219@users.noreply.github.com>
2022-09-20 16:52:09 -0700

    Update Footer.test.js

--- src/compnonents/Footer.test.js
+++ src/compnonents/Footer.test.js
@@ -3,13 +3,10 @@ import Footer from './Footer'
 
 describe('<Footer/>', () => {
     test('renders the Footer page', () => {
-        //Arrange
-        render(<Footer/>) 
-        //Act -
-        //can be interactions like clicking
-        //be passive actions like seeing something       
+        
+        render(<Footer/>       
         const element = screen.getByText(/Francisco Solis & Elijah Barreto/i)
-        // Assert
+      
         expect(element).toBeInTheDocument()
     })
 })
