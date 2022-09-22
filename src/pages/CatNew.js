e28d5d8573f7f564ca394a34a2138c9aa6b52e66
fpsolis219 <fpsolis21995@gmail.com>
2022-09-22 12:01:22 -0700

    Create functionality complete. need to test

--- src/pages/CatNew.js
+++ src/pages/CatNew.js
@@ -1,8 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Form, FormGroup, Input, Label, Button } from "reactstrap"
+import { useNavigate } from 'react-router-dom'
 
-const CatNew = () => {
+
+
+const CatNew = ( { createCat }) => {
+  const navigate = useNavigate()
+  const [newCat, setNewCat] = useState({
+    name: "",
+    age: "",
+    enjoys: "",
+    image: ""
+  })
+
+  const handleChange = (e) => {
+  setNewCat({ ...newCat, [e.target.name]: e.target.value} 
+    )
+}
+
+  const handleSubmit = () => {
+  createCat(newCat)
+  navigate("/CatIndex")
+}
+
   return (
-    <div>CatNew</div>
+    <div>Create a Profile
+      <Form>
+        <FormGroup>
+          <Label for="name">Name</Label>
+          <Input type="text" name="name" placeholder='name' onChange={handleChange} value={newCat.name} />
+        </FormGroup>
+
+        <FormGroup>
+          <Label for="age">age</Label>
+          <Input type="text" name="age" placeholder='age' onChange={handleChange} value={newCat.age} />
+        </FormGroup>
+
+        <FormGroup>
+          <Label for="enjoys">What does your Cat enjoy!</Label>
+          <Input type="text" name="enjoys" placeholder='Describe your Cat for everyone!'onChange={handleChange} value={newCat.enjoys} />
+        </FormGroup>
+
+        <FormGroup>
+          <Label for="image">Profile Pic</Label>
+          <Input type="text" name="image" placeholder='place url' onChange={handleChange} value={newCat.image} />
+        </FormGroup>
+
+        <Button onClick={handleSubmit} name="submit">
+           Create New Cat Profile!
+        </Button>
+        </Form>
+    </div>
   )
 }
 
---
+++ src/pages/CatNew.test.js
@@ -0,0 +1,9 @@
+import { screen, render } from '@testing-library/react'
+import CatNew from './CatNew'
+
+
+describe("<CatNew/>", ()=>{
+  test("renders create profile page without crashing", ()=>{
+    expect()
+  })
+})
