eac6e95f57bb881daaaef7f8df33a872c2fe80b5
Elijah Barreto <51147388+ElijahBcodes@users.noreply.github.com>
2022-09-23 14:40:33 -0700

    Update CatEdit.js

--- src/pages/CatEdit.js
+++ src/pages/CatEdit.js
@@ -26,34 +26,33 @@ const handleSubmit = () => {
 
   return (
     <>
-    <div>Update Cat Profile</div>
-    <Form>
-        <FormGroup>
-          <Label for="name">Edit Name</Label>
-          <Input type="text" name="name" placeholder='name' onChange={handleChange} value={editCat.name} />
-        </FormGroup>
+      <div>Update Cat Profile
+      </div>
+        <Form>
+            <FormGroup>
+              <Label for="name">Edit Name</Label>
+              <Input type="text" name="name" placeholder='name' onChange={handleChange} value={editCat.name} />
+            </FormGroup>
 
-        <FormGroup>
-          <Label for="age">Edit age</Label>
-          <Input type="text" name="age" placeholder='age' onChange={handleChange} value={editCat.age} />
-        </FormGroup>
+            <FormGroup>
+              <Label for="age">Edit age</Label>
+              <Input type="text" name="age" placeholder='age' onChange={handleChange} value={editCat.age} />
+            </FormGroup>
 
-        <FormGroup>
-          <Label for="enjoys">Edit description</Label>
-          <Input type="text" name="enjoys" placeholder='Describe your Cat for everyone!'onChange={handleChange} value={editCat.enjoys} />
-        </FormGroup>
+            <FormGroup>
+              <Label for="enjoys">Edit description</Label>
+              <Input type="text" name="enjoys" placeholder='Describe your Cat for everyone!'onChange={handleChange} value={editCat.enjoys} />
+            </FormGroup>
 
-        <FormGroup>
-          <Label for="image">Edit Profile Pic</Label>
-          <Input type="text" name="image" placeholder='place url' onChange={handleChange} value={editCat.image} />
-        </FormGroup>
-        
-        <Button onClick={handleSubmit} name="submit">
-           Edit Cat
-        </Button>
-        
+            <FormGroup>
+              <Label for="image">Edit Profile Pic</Label>
+              <Input type="text" name="image" placeholder='place url' onChange={handleChange} value={editCat.image} />
+            </FormGroup>
 
-      </Form>
+            <Button onClick={handleSubmit} name="submit">
+               Edit Cat
+            </Button>
+          </Form>
     </>
   )
 }
